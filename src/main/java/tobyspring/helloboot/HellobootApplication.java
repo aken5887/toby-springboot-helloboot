1c4c245ace1437c301dd500576b9a08486a0d557
aken <aken@kakao.com>
2023-02-03 18:01:26 +0900

    서블릿 등록

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,6 +1,10 @@
 package tobyspring.helloboot;
 
-import org.apache.catalina.LifecycleException;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -8,7 +12,16 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 public class HellobootApplication {
   public static void main(String[] args) {
     ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-    WebServer webServer = factory.getWebServer();
+    WebServer webServer = factory.getWebServer(servletContext -> {
+      servletContext.addServlet("hello", new HttpServlet() {
+        @Override
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          resp.setStatus(200);
+          resp.setHeader("Content-Type","text-plain");
+          resp.getWriter().println("Hello Servlet");
+        }
+      }).addMapping("/hello");
+    });
     webServer.start();
   }
 }
