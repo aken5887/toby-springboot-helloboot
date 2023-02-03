f83c07d249bef508c30a98ddc1bb56b9b1605462
aken <aken@kakao.com>
2023-02-03 17:31:37 +0900

    빈 서블릿 컨테이너 생성

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,7 +1,15 @@
 package tobyspring.helloboot;
 
-public class HellobootApplication {
+import org.apache.catalina.LifecycleException;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
+public class HellobootApplication {
   public static void main(String[] args) {
+    ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+    WebServer webServer = factory.getWebServer();
+    webServer.start();
   }
 }
+
