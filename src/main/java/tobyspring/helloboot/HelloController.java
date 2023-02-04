3aa46ed9c92e9614af885eaa73173d6503166ccb
aken <aken@kakao.com>
2023-02-04 16:41:41 +0900

    DispatcherServlet 전환

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,5 +1,13 @@
 package tobyspring.helloboot;
 
+import java.util.Objects;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
 public class HelloController {
   private final HelloService helloService;
 
@@ -7,7 +15,9 @@ public class HelloController {
     this.helloService = helloService;
   }
 
+  @GetMapping("/hello")
+  @ResponseBody
   public String hello(String name) {
-    return helloService.sayHello(name);
+    return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,44 +1,23 @@
 package tobyspring.helloboot;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
   public static void main(String[] args) {
-    GenericApplicationContext applicationContext = new GenericApplicationContext();
+    /** Spring Container **/
+    GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.refresh(); // 애플리케이션 컨텍스트 초기화
 
     ServletWebServerFactory factory = new TomcatServletWebServerFactory();
     WebServer webServer = factory.getWebServer(servletContext -> {
-      servletContext.addServlet("frontcontroller", new HttpServlet() {
-        @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-          // 인증, 보안, 다국어, 공통 기능
-          if("GET".equals(req.getMethod()) && "/hello".equals(req.getRequestURI())){
-            String name = req.getParameter("name");
-            HelloController helloController = applicationContext.getBean(HelloController.class);
-            String ret = helloController.hello(name); // 작업을 위임
-
-            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().println(ret);
-          }
-          else{
-            resp.setStatus(HttpStatus.BAD_REQUEST.value());
-          }
-        }
-      }).addMapping("/*");
+      servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+          .addMapping("/*");
     });
     webServer.start();
   }
