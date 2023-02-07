455c7be0e3be1f8449374468d5ffa8aabd1febc2
aken <aken@kakao.com>
2023-02-07 18:39:06 +0900

    커스텀 @Conditional 애노테이션

--- src/main/java/tobyspring/config/autoconfig/JettyServletWebServerFactoryConfig.java
+++ src/main/java/tobyspring/config/autoconfig/JettyServletWebServerFactoryConfig.java
@@ -5,23 +5,15 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyServletWebServerFactoryConfig.JettyCondition.class)
+@ConditionalMyOnClass(value="org.eclipse.jetty.server.Server", use =true)
 public class JettyServletWebServerFactoryConfig {
-
   @Bean("jettyWebServerFactory")
   public ServletWebServerFactory jettyServletWebServerFactory(){
     return new JettyServletWebServerFactory();
-  }
-
-  static class JettyCondition implements Condition {
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return true;
-    }
   }
 }
--- src/main/java/tobyspring/config/autoconfig/TomcatServletWebServerFactoryConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatServletWebServerFactoryConfig.java
@@ -1,28 +1,19 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(TomcatServletWebServerFactoryConfig.TomcatCondition.class)
+@ConditionalMyOnClass(value="org.apache.catalina.startup.Tomcat", use =false)
 public class TomcatServletWebServerFactoryConfig {
-
   @Bean("tomcatWebServerFactory")
   public ServletWebServerFactory tomcatServletWebServerFactory(){
     return new TomcatServletWebServerFactory();
-  }
-
-  static class TomcatCondition implements Condition {
-    @Override
-    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-      return false;
-    }
   }
 }
